09a5579d730272ab66147f1161e8ebd95a8f2005
tsanjog <sanjogthapa319@gmail.com>
2021-05-27 17:14:39 +0545

    Resume Project

---
+++ resumeproject/miniblog/simpleblog/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import Post
+
+
+# Register your models here.
+#admin.site.register(Post)
+
+@admin.register(Post)
+class PostModelAdmin(admin.ModelAdmin):
+    list_display = ['id','title', 'desc']
